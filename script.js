52d6d9fb8c9c1e2435da2042526570a526dfebb0
Wesley Welisten <welistenw@gmail.com>
2023-01-11 11:47:58 -0300

    add link scroll-to-top

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const navbarEl = document.querySelector(".navbar"),
       bottomContainerEl = document.querySelector(".bottom-container"),
       logoEl = document.querySelector("#logo"),
-      searchFielEl = document.querySelector(".search-field")
+      searchFielEl = document.querySelector(".search-field"),
+      scrollTop = document.querySelector(".scroll-to-top")
 
       setPage()
 
@@ -11,16 +12,24 @@ window.addEventListener("scroll", ()=>{
         navbarEl.classList.add("active")
         searchFielEl.classList.add("active")
         logoEl.setAttribute('src', 'imgs/logoWhite.svg')
+        scrollTop.classList.add("active")
     
     }else{
         navbarEl.classList.remove("active")
         searchFielEl.classList.remove("active")
         logoEl.setAttribute('src', 'imgs/logoGreen.svg')
-
+        scrollTop.classList.remove("active")
     }
+
+    scrollTop.addEventListener("click", (e) => {
+        e.preventDefault()
+        window.scrollTo(0,0)
+    })
+
 
 
 })
+
  function setPage( ) {
     navbarEl.classList.remove("active")
     searchFielEl.classList.remove("active")
